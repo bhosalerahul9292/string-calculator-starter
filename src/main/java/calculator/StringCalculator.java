daf729888a12544a3a6b4cb5b4c9bb50de809a50
Rahul Bhosale <bhosalerahul5920@gmail.com>
2021-10-16 21:47:41 +0530

    fix - failing initial test case

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,10 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	if(input.length()==1) {
+			return Integer.parseInt(input);
+		}
+    	return 0;
     }
 
 }
